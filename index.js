5d2f620ea06c86b06eb7b4d1807a896f0f78a5a7
Milo Rastgoo <hi@informagician.com>
2020-02-26 18:29:22 -0500

    register endpoint working

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
-
+const Users = require('./model/users')
 const server = express();
 
-server.use = express.json();
+server.use(express.json());
 
 server.get('/', (req,res) => {
     res.status(200).json({message:'its working!'})
+})
+
+server.post('/api/register', (req,res) => {
+    const user = req.body
+    //console.log(user)
+    Users.register(user)
+    .then(id => {
+        res.status(201).json(id)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({errorMessage:'Something went wrong during register'})
+    })
+})
+
+server.post('/api/login', (req,res) => {
+    const user = req.body
+    Users.login(user)
+    .then(id => {
+        res.status(201).json(id)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({errorMessage:'Something went wrong during register'})
+    })
 })
 
 const port = 5000;
---
+++ model/users.js
@@ -0,0 +1,33 @@
+const knex = require('knex')
+const db = knex(require('../database/knexfile').development)
+
+module.exports = {
+    register,
+    login,
+    list
+}
+
+function register(user) {
+    return db('users')
+    .insert(user, 'id')
+}
+
+function login(user){
+    return db('users')
+    .select('*')
+    .where({username:user.username})
+    .first()
+}
+
+function list() {
+    return db('users')
+    .select('*')
+}
+
+// function findById(id) {
+
+//     db('users')
+//     .select('*')
+//     .where({id})
+//     .first()
+// }
